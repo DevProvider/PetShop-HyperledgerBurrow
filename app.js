22e1d09e96530ebeb6371cf35a07570290bbe6f3
Khuram <khuram@devprovider.com>
2019-04-17 17:52:51 +0500

    packages updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 var _http = require('http');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const exec = require('child_process').exec;
+const cmd = require('node-cmd');
  
 app.use(express.static('src'));
   
@@ -10,20 +12,67 @@ http.listen(3000, function () {
     console.log('Check out the petstore at http://localhost:3000');
 });
  
-var addresses = [0];
-
+var addresses = [];
+var command = '';
 io.on('connection', function (socket) {
-    
-    io.emit('adopted', addresses);
+    command = 'snak call Adoption getAdopters'
+	exec(command,function(err,stdout,stderr){
+		if(!err){
 
+			var array = [];
+			array = JSON.parse(stdout);
+			for (var i = 0; i < array.length; i++ )
+			{
+				if (array[i].toString() == '8B05651240631E5724CC380D7D249C1FC63F320B')
+				{
+					addresses.push(i);
+				}
+			}		
+		}
+
+		else{
+			console.log(stderr);
+		}
+	});
+    io.emit('adopted', addresses);
+ 
     socket.on('adopt', function (msg) {
-    	if (addresses.indexOf(parseInt(msg)) < 0){
-        	addresses.push(msg);
-        	io.emit('adopted', addresses);
-    	}
-    	console.log(addresses);
+    	command = 'snak call Adoption adopt '+ msg.toString();
+    	console.log(command);
+    	exec(command, async function(err,stdout,stderr){
+    		if(err){
+    			console.log(stderr);
+    		}
+    		else
+    		{
+				command = 'snak call Adoption getAdopters'
+				exec(command,function(err,stdout,stderr){
+					if(!err){
+
+						var array = [];
+						array = JSON.parse(stdout);
+						for (var i = 0; i < array.length; i++ )
+						{
+							if (array[i].toString() == '8B05651240631E5724CC380D7D249C1FC63F320B')
+							{
+								addresses.push(i);
+							}
+						}	
+
+						io.emit('adopted', addresses);
+	
+					}
+
+					else{
+						console.log(stderr);
+					}
+				});	    			
+    		}
+			
+		});
     });
 });
+
 
 io.on('disconnect', function(socket) {
 	console.log('socket disconeceted');
